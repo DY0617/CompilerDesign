3a510484116641f88985d520d49699cca0fb4b1d
dongyeop <brassdong@gmail.com>
2022-05-13 17:56:41 +0900

    LLparser

--- hw10/src/Parser/Main.java
+++ hw10/src/Parser/Main.java
@@ -2,8 +2,8 @@ package Parser;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
